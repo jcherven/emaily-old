26a439c8cade4f0fc27f8e63e03bbb033fb6458c
James Cherven <jcherven@mail.usf.edu>
2019-07-28 13:26:54 -0400

    Adds a mongodb model for users

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('./models/User');
 require('./services/passport');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true});
 
 const app = express();
 
