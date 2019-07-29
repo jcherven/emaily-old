49db89018fcc5bbc3df4614b5857724db09d89a5
James Cherven <jcherven@mail.usf.edu>
2019-07-29 00:26:07 -0400

    Fixes prod keys file

--- config/prod.js
+++ config/prod.js
@@ -3,7 +3,5 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKOE_KEY,
+  cookieKey: process.env.COOKIE_KEY,
 };
-
-mongodb+srv://emaily-prod:2bkub4fly4Q4mQHy@cluster0-xbayl.mongodb.net/test?retryWrites=true&w=majority
