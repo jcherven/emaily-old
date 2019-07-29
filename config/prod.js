8c265b2a6d138bcd754c7f3ae14e6aece4985a86
James Cherven <jcherven@mail.usf.edu>
2019-07-29 00:07:30 -0400

    Adds dev and prod env configuration

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+// prod.js
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKOE_KEY,
+};
+
+mongodb+srv://emaily-prod:2bkub4fly4Q4mQHy@cluster0-xbayl.mongodb.net/test?retryWrites=true&w=majority
