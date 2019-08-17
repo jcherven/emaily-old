45dafb06f01ecdc1b2748d55da771b7d04cd499b
James Cherven <jcherven@mail.usf.edu>
2019-08-17 18:56:50 -0400

    Adds styling library materialize-css; builds a little bit of the header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+// Components
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
